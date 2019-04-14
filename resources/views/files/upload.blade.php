b750067046e9a02abe978b87d2af785bb0bae11e
the-gero <47275336+the-gero@users.noreply.github.com>
2019-04-14 12:12:31 +0530

    few more minor adjustments

--- resources/views/files/upload.blade.php
+++ resources/views/files/upload.blade.php
@@ -1,21 +1,25 @@
 @extends('layouts.app')
 @section('content')
-<div class="row  " style="margin-top:0vh" align=center>
+
+<div class="row" align=center>
 {!! Form::open(['action' => 'FilesController@store', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
-        
-        <div class="form-group col-md-3">
-        {{Form::file('file')}}
-        </div>
-        <div class="col-md-3">
-       <button type="submit" onclick="return confirm('Are you sure to Upload this file ?');" style="border: 0; background: none;">
+        <button type="submit" onclick="return confirm('Are you sure to Upload this file ?');" style="border: 0; background: none;">
           
               <a href="/file/create"><h1 style="font-size:6rem; color:white;" align=center><i class="fa fa-upload "></i></h1>
                 
               </a><h3 style="font-size:1rem; color:white;" align=center>Click here to confirm upload</h3>
               
           </button>
+        <div class="form-group ">
+            <div class="form-group files">
+                <label>Upload Your File </label>
+                <input type="file" class="form-control" multiple="">
+            </div>
+        </div>
+   
+       
     {!! Form::close() !!}
-</div>
+
 </div>
 @endsection
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,49 +1,43 @@
 @extends('layouts.app')
 @section('content')
-<div class="card">
-    <div class="card-header text-center" style="height:10vh;background:grey;"> <h2 style="color:black">Welcome to InAir</h2>
-        
-    </div>
-        <div class="card-body" style="background:#55585c80" >
-            <div id="demo" class="carousel slide" data-ride="carousel">
-    <ul class="carousel-indicators">
-      <li data-target="#demo" data-slide-to="0" class="active"></li>
-      <li data-target="#demo" data-slide-to="1"></li>
-      <li data-target="#demo" data-slide-to="2"></li>
-    </ul>
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="{{ asset("students.jpg") }}" alt="Los Angeles" width="1100" height="500">
-        <div class="carousel-caption">
-            <h3 style="color:lightpink">We provide cloud strorage and sharing for students </h3>
-          <p>Get started now!</p>
-        </div>   
-      </div>
-      <div class="carousel-item">
-        <img src="{{ asset("security.png") }}" alt="Chicago" width="1100" height="500">
-        <div class="carousel-caption">
-          <h3>Your files are secure</h3>
-          <p>Get your ID in seconds and share files with your friends !</p>
-        </div>   
-      </div>
-      <div class="carousel-item">
-        <img src="{{ asset("multidevice.png") }}" alt="New York" width="1100" height="500">
-        <div class="carousel-caption">
-          <h3>New York</h3>
-          <p>We love the Big Apple!</p>
-        </div>   
-      </div>
-    </div>
-    <a class="carousel-control-prev" href="#demo" data-slide="prev">
-      <span class="carousel-control-prev-icon"></span>
-    </a>
-    <a class="carousel-control-next" href="#demo" data-slide="next">
-      <span class="carousel-control-next-icon"></span>
-    </a>
-  </div>
-        </div>
-    </div>
 
+    <div id="demo" class="carousel slide p-2 bd-highlight" data-ride="carousel" style="max-height: 60vh;max-width: 50vw;" align=center>
+      <ul class="carousel-indicators">
+        <li data-target="#demo" data-slide-to="0" class="active"></li>
+        <li data-target="#demo" data-slide-to="1"></li>
+        <li data-target="#demo" data-slide-to="2"></li>
+      </ul>
+      <div class="carousel-inner" style="max-height: 60vh;max-width: 50vw;border-radius:10px;">
+        <div class="carousel-item active">
+          <img src="{{ asset("students.jpg") }}" alt="Los Angeles" style="height: 60vh;width: 50vw;border-radius:10px;">
+          <div class="carousel-caption">
+              <h3 style="background:black; border-radius:3px;">We provide cloud strorage and sharing for students </h3>
+            <p style="background:black; border-radius:3px;">Get started now!</p>
+          </div>   
+        </div>
+        <div class="carousel-item">
+          <img src="{{ asset("security.png") }}" alt="Chicago" style="height: 60vh;width: 50vw; border-radius:10px;">
+          <div class="carousel-caption">
+            <h3 style="background:black; border-radius:3px;">Your files are secure</h3>
+            <p style="background:black; border-radius:3px;">Get your ID in seconds and share files with your friends !</p>
+          </div>   
+        </div>
+        <div class="carousel-item">
+          <img src="{{ asset("multidevice.png") }}" alt="Access From any device" style="height: 60vh;width: 50vw;border-radius:10px;">
+          <div class="carousel-caption">
+            <h3 style="background:black; border-radius:3px;">Access from any device</h3>
+            <p style="background:black; border-radius:3px;">Android, iOS , Windows , Linux ! </p>
+            <p style="background:black; border-radius:3px;">Anytime anywhere</p>
+          </div>   
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#demo" data-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+      </a>
+      <a class="carousel-control-next" href="#demo" data-slide="next">
+        <span class="carousel-control-next-icon"></span>
+      </a>
+    </div>
 
 
     
